86f2a5e3dad9c5cfe3210c6032ae62281eea7950
Mathggg <75087702+Mathggg@users.noreply.github.com>
2023-04-05 19:42:01 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+let text_tokens = [];
+
+
 window.onload = function() {
     let fileInput = document.getElementById('fileInput');
     let fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -21,13 +24,13 @@ window.onload = function() {
             // dans la zone d'affichage du texte.
             reader.onload = function(e) {
                 fileDisplayArea.innerText = reader.result;
+                segmentation();
+                document.getElementById("logger").innerHTML = '<span class="infolog">Fichier chargé avec succès, ' + text_tokens.length + ' tokens dans le texte.</span>';
             }
 
             // on lit concrètement le fichier.
             // Cette lecture lancera automatiquement la fonction "onload" juste au-dessus.
-            reader.readAsText(file);    
-
-            document.getElementById("logger").innerHTML = '<span class="infolog">Fichier chargé avec succès</span>';
+            reader.readAsText(file);
         } else { // pas un fichier texte : message d'erreur.
             fileDisplayArea.innerText = "";
             document.getElementById("logger").innerHTML = '<span class="errorlog">Type de fichier non supporté !</span>';
@@ -36,48 +39,97 @@ window.onload = function() {
 }
 
 
-window.onload = function segmentText() {
-	let text = document.getElementByld("texte").value;
-	const mots = text.split(new RegExp("[,\"'.]"));
-	document.getElementByld("holder2).innerHTML=mots;
-}
-
-window.onload = function tokens() {
-import nltk
-
-text = text_segmentation
-tokens = nltk.word_tokenize(text)
-print(tokens)
-}
-
-window.onload = function globalTokens() {
-	import nltk
-	def tokenize_text(text):
-    global global_tokens
-    doc = nlp(text)
-    global_tokens = [token.text for token in doc]
-	tokenize_text(my_text)
-	print(global_tokens)
+function afficheCacheAide() {
+    let aide = document.getElementById("aide");
+    let boutonAide = document.getElementById("boutonAide");
+    let display = aide.style.display;
+    
+    if (display === "none") {
+        aide.style.display = "block";
+        boutonAide.innerText = "Cacher l'aide";
+    } else {
+        aide.style.display = "none";
+        boutonAide.innerText = "Afficher l'aide";
+    }
 }
 
 
-window.onload = function globalTokens() {
-def dictionnaire(tokens):
-    token_counts = Counter(global_tokens)
+function segmentation() {
+    let text = document.getElementById("fileDisplayArea").innerText;
+    let delim = document.getElementById("delimID").value;
+    let display = document.getElementById("page-analysis");
 
-    sorted_dict = dict(sorted(token_counts.items(), key=lambda x: x[1], reverse=True))
-    return sorted_dict
+    let regex_delim = new RegExp(
+        "["
+        + delim
+            .replace("-", "\\-") // le tiret n'est pas à la fin : il faut l'échapper, sinon erreur sur l'expression régulière
+            .replace("[", "\\[").replace("]", "\\]") // à changer sinon regex fautive, exemple : [()[]{}] doit être [()\[\]{}], on doit "échapper" les crochets, sinon on a un symbole ] qui arrive trop tôt.
+        + "\\s" // on ajoute tous les symboles d'espacement (retour à la ligne, etc)
+        + "]+" // on ajoute le + au cas où plusieurs délimiteurs sont présents : évite les tokens vides
+    );
 
-my_dict = dictionnaire(global_tokens)
-print(my_dict)
+    let tokens_tmp = text.split(regex_delim);
+    text_tokens = tokens_tmp.filter(x => x.trim() != ''); // on s'assure de ne garder que des tokens "non vides"
 
-def Dictionnaire_click():
-    global tokens_entry, results_text
-    tokens = tokens_entry.get().split()
-	 results_text.delete("1.0", tk.END)
-    for key, value in my_dict.items():
-        results_text.insert(tk.END, f"{key}: {value}\n")
+    // global_var_tokens = tokens; // décommenter pour vérifier l'état des tokens dans la console développeurs sur le navigateur
+    // display.innerHTML = tokens.join(" ");
 }
 
+
+function dictionnaire() {
+    let comptes = new Map();
+    let display = document.getElementById("page-analysis");
+
+    for (let token of text_tokens) {
+        comptes.set(token, (comptes.get(token) ?? 0) + 1);
+    }
+    
+    let comptes_liste = Array.from(comptes);
+    comptes_liste = comptes_liste.sort(function(a, b) {
+        // solution attendue
+        return b[1] - a[1]; // tri numérique inversé
+
+        /*
+         * // solution alternative
+         * // on trie sur les comptes en priorité
+         * // puis, pour les comptes identiques, on trie sur la forme
+         * let a_form = a[0];
+         * let a_count = a[1];
+         * let b_form = b[0];
+         * let b_count = b[1];
+         * let comparaison = 0;
+         *
+         * // utiliser +2 et -2 permet de donner plus de poids aux comptes (permet le trie du plus fréquent au moins fréquent)
+         * if (a_count < b_count) {
+         *     comparaison += 2;
+         * } else if (a_count > b_count) {
+         *     comparaison -= 2;
+         * }
+         * // -1 et +1 permettent d'ajuster le tri en cas de comptes égaux, mais ne peut pas inverser l'ordre pour des comptes différents
+         * if (a_form < b_form) {
+         *     comparaison -= 1;
+         * } else if (a_form > b_form) {
+         *     comparaison += 1;
+         * }
+         *
+         * return comparaison;
+         */
+    });
+
+    let table = document.createElement("table");
+    table.style.margin = "auto";
+    let entete = table.appendChild(document.createElement("tr"));
+    entete.innerHTML = "<th>mot</th><th>compte</th>";
+    
+    for (let [mot, compte] of comptes_liste) {
+        let ligne_element = table.appendChild(document.createElement("tr"));
+        let cellule_mot = ligne_element.appendChild(document.createElement("td"));
+        let cellule_compte = ligne_element.appendChild(document.createElement("td"));
+        cellule_mot.innerHTML = mot;
+        cellule_compte.innerHTML = compte;
+    }
+
+    display.appendChild(table);
+}
 
 
